2c26f59c4b665172ee9966330e02901113eaeaa3
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 13:44:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,4 @@ def display_rainbow(colors, index)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
