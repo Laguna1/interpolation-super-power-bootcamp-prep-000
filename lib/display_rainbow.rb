5b9c8daeb2e4df05e91326ca1a784813459e2aea
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 13:42:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(colors)
+def display_rainbow(colors, index)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
