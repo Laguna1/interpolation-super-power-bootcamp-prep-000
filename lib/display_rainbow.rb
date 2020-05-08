b50496469466a0e3c1a18b8918a2ea48c16a6d81
Oksana Petrova <lagunazoomarket@gmail.com>
2020-05-08 13:49:57 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors, index)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, "
   puts "Contact: #{person[3]}"
 end
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow(colors)
